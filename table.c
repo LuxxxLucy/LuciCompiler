5dc3c5f273f8ab915f337c763c3387adc67b0773
陆家林 <3140102299@zju.edu.cn>
2017-05-22 20:48:35 +0800

    table type convertion
    fix the hashing function error

--- table.c
+++ table.c
@@ -4,7 +4,7 @@
  */
 
 #include <stdio.h>
-#include "util.h"
+#include "utility.h"
 #include "table.h"
 
 #define TABSIZE 127
@@ -18,14 +18,14 @@ struct TAB_table_ {
 
 
 static binder Binder(void *key, void *value, binder next, void *prevtop)
-{binder b = checked_malloc(sizeof(*b));
- b->key = key; b->value=value; b->next=next; b->prevtop = prevtop; 
+{binder b = (binder)checked_malloc(sizeof(*b));
+ b->key = key; b->value=value; b->next=next; b->prevtop = prevtop;
  return b;
 }
 
 TAB_table TAB_empty(void)
-{ 
- TAB_table t = checked_malloc(sizeof(*t));
+{
+ TAB_table t = (TAB_table) checked_malloc(sizeof(*t));
  int i;
  t->top = NULL;
  for (i = 0; i < TABSIZE; i++)
@@ -45,7 +45,7 @@ TAB_table TAB_empty(void)
 void TAB_enter(TAB_table t, void *key, void *value)
 {int index;
  assert(t && key);
- index = ((unsigned)key) % TABSIZE;
+ index = ( reinterpret_cast<unsigned long>(key)) % TABSIZE;
  t->table[index] = Binder(key, value,t->table[index], t->top);
  t->top = key;
 }
@@ -54,7 +54,7 @@ void *TAB_look(TAB_table t, void *key)
 {int index;
  binder b;
  assert(t && key);
- index=((unsigned)key) % TABSIZE;
+ index=((unsigned long)key) % TABSIZE;
  for(b=t->table[index]; b; b=b->next)
    if (b->key==key) return b->value;
  return NULL;
@@ -65,7 +65,7 @@ void *TAB_pop(TAB_table t) {
   assert (t);
   k = t->top;
   assert (k);
-  index = ((unsigned)k) % TABSIZE;
+  index = ((unsigned long)k) % TABSIZE;
   b = t->table[index];
   assert(b);
   t->table[index] = b->next;
@@ -75,7 +75,7 @@ void *TAB_pop(TAB_table t) {
 
 void TAB_dump(TAB_table t, void (*show)(void *key, void *value)) {
   void *k = t->top;
-  int index = ((unsigned)k) % TABSIZE;
+  int index = ((unsigned long)k) % TABSIZE;
   binder b = t->table[index];
   if (b==NULL) return;
   t->table[index]=b->next;
